dd70baa65c01f019c36a40d33ce639ac6a1d2c2b
gray-kim <egoist3@gmail.com>
2021-09-08 10:17:36 +0900

    use vue3

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,18 +1,39 @@
 <template>
   <h1>{{ msg }}</h1>
-  <button @click="count++">count is: {{ count }}</button>
+<!--  <button @click="count++">count is: {{ count }}</button>-->
+  <button @click="plusCount();">count is: {{ state.count }}</button>
   <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
 </template>
 
 <script>
+import { reactive } from 'vue'
+// export default {
+//   name: 'HelloWorld',
+//   props: {
+//     msg: String
+//   },
+//   data() {
+//     return {
+//       count: 0
+//     }
+//   }
+// }
+
+// Vue 3.x
 export default {
-  name: 'HelloWorld',
   props: {
     msg: String
   },
-  data() {
-    return {
+  setup () {
+    const state = reactive({
       count: 0
+    })
+    const plusCount =() => {
+      state.count++;
+    }
+    return {
+      plusCount,
+      state
     }
   }
 }
