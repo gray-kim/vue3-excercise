47c0ae5bd4270b90db8a5ade686df396ea511f81
gray-kim <egoist3@gmail.com>
2021-09-08 13:58:25 +0900

    add vue3 source and comment

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,32 +6,83 @@
 </template>
 
 <script>
-import { reactive } from 'vue'
+// Vue 2.x
 // export default {
 //   name: 'HelloWorld',
 //   props: {
-//     msg: String
+//     msg: String,
+//     title: String
+//   },
+//   computed: {
+//     lowerCaseUsername () {
+//       return this.username.toLowerCase()
+//     }
+//   },
+//   mounted () {
+//     console.log('title: ' + this.title)
 //   },
 //   data() {
 //     return {
-//       count: 0
+//       count: 0,
+//       username: ''
 //     }
 //   }
 // }
 
 // Vue 3.x
+// 다양한 옵션을 함수 형태로 제공
+import { reactive } from 'vue'
+import { computed } from 'vue'
+import { onBeforeMount, onMounted, onBeforeUpdate, onUpdated, onBeforeUnmount, onUnmounted, onActivated, onDeactivated, onErrorCaptured } from 'vue'
+
 export default {
   props: {
-    msg: String
+    msg: String,
+    title: String
   },
-  setup () {
-    const state = reactive({
-      count: 0
+  setup (props) { // setup에서 props 사용 시 props를 attribute로 받음. 기존 2.x에서 this.으로 접근하던 방식에서 변화함
+    // lifecycle hook
+    onBeforeMount(() => {
+      // ...
     })
+    onMounted(() => {
+      console.log('title: ' + props.title)
+    })
+    onBeforeUpdate(() => {
+      // ...
+    })
+    onUpdated(() => {
+      // ...
+    })
+    onBeforeUnmount(() => {
+      // ...
+    })
+    onUnmounted(() => {
+      // ...
+    })
+    onActivated(() => {
+      // ...
+    })
+    onDeactivated(() => {
+      // ...
+    })
+    onErrorCaptured(() => {
+      // ...
+    })
+
+    // data 선언부
+    const state = reactive({  // 반응형(reactive) 데이터 바인딩
+      count: 0,
+      username: '',
+      lowerCaseUsername: computed(() => state.username.toLowerCase())
+    })
+
+    // method 선언부
     const plusCount =() => {
       state.count++;
     }
     return {
+      // lifecycle은 return하지 않음
       plusCount,
       state
     }
