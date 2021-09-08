55686a12a33db2820ba051dee3e0eda1dec881da
gray-kim <egoist3@gmail.com>
2021-09-08 16:56:24 +0900

    add vue3 source and comment

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,20 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3.0 + Vite" title="vue3 title" />
+  <MyCheckbox :checked="foo"
+              @change="val => {foo = val}"
+              value="some value" />
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import MyCheckbox from './components/MyCheckbox.vue'
 
 export default {
-  name: 'App',
+  name: 'Vue3 App Test',
   components: {
-    HelloWorld
+    HelloWorld,
+    MyCheckbox
   }
 }
 </script>
---
+++ src/components/MyCheckbox.vue
@@ -0,0 +1,24 @@
+<template>
+  <input type="checkbox" v-model="foo" value="any value"/>
+</template>
+
+<script>
+export default {
+  name: "MyCheckbox",
+  props: {
+    value: String,
+    checked: {
+      type: Number,
+      default: 0
+    }
+  },
+  model: {
+    prop: 'checked',
+    event: 'change'
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
