8bfb1bdbe0497806271f659d81543ea0a688f3bf
gray-kim <egoist3@gmail.com>
2021-09-24 17:14:26 +0900

    add kendo button component

--- src/components/KendoGridCustom.vue
+++ src/components/KendoGridCustom.vue
@@ -3,27 +3,33 @@
     <dropdownlist
         :data-items="data.categories"
         :data-item-key="'CategoryID'"
-        :text-field="'CategoryName'"
-    >
+        :text-field="'CategoryName'">
     </dropdownlist>
+    <k-button>Default</k-button>
   </p>
   <grid
       :data-items="data.products"
-      :columns="data.columns"
-  ></grid>
+      :columns="data.columns">
+    <grid-norecords>
+      There is no data available custom
+    </grid-norecords>
+  </grid>
 </template>
 
 <script>
 import { process } from '@progress/kendo-data-query';
-import { Grid } from '@progress/kendo-vue-grid';
+import { Grid, GridNoRecords } from '@progress/kendo-vue-grid';
 import { DropDownList } from '@progress/kendo-vue-dropdowns';
+import { Button, ButtonGroup, ChipList, FloatingActionButton } from '@progress/kendo-vue-buttons';
 import '@progress/kendo-theme-default/dist/all.css';
 
 export default {
   name: "KendoGridCustom",
   components: {
+    'k-button': Button,
     'dropdownlist': DropDownList,
     'grid': Grid,
+    'grid-norecords': GridNoRecords
   },
   props: {
     data: {
